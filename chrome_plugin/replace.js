a11e1969a3cd84c3589f8e14a467fa11da5b2d2a
Mendbayar <mrmendee@gmail.com>
2014-09-13 22:54:53 +0900

    Loaded
    Bolgoh

--- chrome_plugin/replace.js
+++ chrome_plugin/replace.js
@@ -1,7 +1,17 @@
-﻿var theText = document.body.innerHTML;
+﻿
+$(function(){
+var theText = document.body.innerHTML;
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var fh = fso.CreateTextFile("E:\\USER\Mende\Desktop\Test.txt");
 // Replace words
 theText = theText.replace("мэдээ", "Х");
-theText = theText.replace("After", "Daraa");
+theText = theText.replace("after", "Daraa");
 theText = theText.replace("Mendbayar", "Menduushka");
+document.WriteLine(theText);	
+fh.WriteLine(theText);
+fh.Close();
 
 document.body.innerHTML = theText;
+
+}
+);
